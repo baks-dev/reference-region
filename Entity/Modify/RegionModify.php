b1eaf088415b42ea1b1715bab12bcb363bf05222
kingstar <admin@baks.dev>
2023-10-25 17:06:46 +0300

    Baks Development

--- Entity/Modify/RegionModify.php
+++ Entity/Modify/RegionModify.php
@@ -28,7 +28,8 @@ namespace BaksDev\Reference\Region\Entity\Modify;
 use BaksDev\Core\Entity\EntityEvent;
 use BaksDev\Core\Type\Ip\IpAddress;
 use BaksDev\Core\Type\Modify\ModifyAction;
-use BaksDev\Core\Type\Modify\ModifyActionEnum;
+use BaksDev\Core\Type\Modify\Modify\ModifyActionNew;
+use BaksDev\Core\Type\Modify\Modify\ModifyActionUpdate;
 use BaksDev\Reference\Region\Entity\Event\RegionEvent;
 use BaksDev\Users\User\Entity\User;
 use BaksDev\Users\User\Type\Id\UserUid;
@@ -80,14 +81,14 @@ class RegionModify extends EntityEvent
 		$this->modDate = new DateTimeImmutable();
 		$this->ip = new IpAddress('127.0.0.1');
 		$this->agent = 'console';
-		$this->action = new ModifyAction(ModifyActionEnum::NEW);
+		$this->action = new ModifyAction(ModifyActionNew::class);
 	}
 	
 	
 	public function __clone() : void
 	{
 		$this->modDate = new DateTimeImmutable();
-		$this->action = new ModifyAction(ModifyActionEnum::UPDATE);
+		$this->action = new ModifyAction(ModifyActionUpdate::class);
 		$this->ip = new IpAddress('127.0.0.1');
 		$this->agent = 'console';
 	}
